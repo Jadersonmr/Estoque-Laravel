a8eddcbc42b04623ae273b376d418ee4063faa0f
Jadersonmr <jaderson.mrodrigues@gmail.com>
2022-02-02 19:51:42 -0300

    adicionado controller de produtos

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        return $this->product = $product;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'index';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return 'create';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->product->create($request->all());
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $this->product->find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $this->product->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $productData = $this->product->find($id);
+        $productData->update($request->all());
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->product->find($id)->delete();
+
+        return redirect()->route('products.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+Route::group(['prefix' => 'produtos', 'as' => 'products.'], function() {
+    Route::get('', 'ProductController2@index')->name('index');
+    Route::get('/criar', 'ProductController2@create')->name('create');
+    Route::post('/criar', 'ProductController2@store')->name('store');
+    Route::get('/{id}/produto', 'ProductController2@show')->name('show');
+    Route::get('/{id}/editar', 'ProductController2@edit')->name('edit');
+    Route::put('/{id}/editar', 'ProductController2@update')->name('update');
+    Route::delete('/{id}/excluir', 'ProductController2@destroy')->name('destroy');
+});
+
+//Route::get('/categorias/{flag}', function ($flag) {
+//   return "Produtos da categoria: {$flag}";
+//});
+
+//Route::match(['post', 'get'], '/match', function () {
+//   return 'match';
+//});
+
 Route::get('/', function () {
     return view('welcome');
 });
