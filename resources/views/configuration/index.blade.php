3cc0ed7521b4e0bb737366bb8858371d468abfac
Jadersonmr <jaderson.mrodrigues@gmail.com>
2022-06-11 19:22:32 -0300

    envia arquivo de configuracoes

--- resources/views/configuration/index.blade.php
+++ resources/views/configuration/index.blade.php
@@ -21,6 +21,8 @@
             integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
             crossorigin=""></script>
     <script src="https://unpkg.com/leaflet.gridlayer.googlemutant@latest/dist/Leaflet.GoogleMutant.js"></script>
+    <script src="{{asset('js/leaflet.rotatedMarker.js')}}"></script>
+    <script src="{{asset('js/leaflet-polylinedecorator/dist/leaflet.polylineDecorator.js')}}"></script>
     <script>
         function success(pos){
             console.log(pos.coords.latitude, pos.coords.longitude);
@@ -52,7 +54,6 @@
         var layerControl = L.control.layers(baseMaps).addTo(map);
 
         map.on('click', function (e){
-            console.log(e.latlng);
             L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
         });
 
@@ -67,8 +68,59 @@
             [-28.478635588933496, -49.01060521602631]
         ];
 
-        var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+        var directionIcon = L.icon({
+            iconUrl: "{{asset('images/a_up_arrow_icon.svg')}}",
+            iconSize:     [38, 95], // size of the icon
+            iconAnchor:   [22, 54], // point of the icon which will correspond to marker's location
+        });
+
+        var startIcon = L.icon({
+            iconUrl: "{{asset('images/maker_map_flag_location_icon.svg')}}",
+            iconSize:     [38, 95], // size of the icon
+            iconAnchor:   [22, 60], // point of the icon which will correspond to marker's location
+        });
+
+        var endIcon = L.icon({
+            iconUrl: "{{asset('images/flag_finish_goal_icon.svg')}}",
+            iconSize:     [38, 95], // size of the icon
+            iconAnchor:   [15, 60], // point of the icon which will correspond to marker's location
+        });
+
+        for(let index = 0; index < latlngs.length; index++) {
+            let angle;
+
+            if(typeof latlngs[index+1] !== "undefined") {
+                angle = getAngle(latlngs[index], latlngs[index + 1], -2);
+            }
+
+            if(index === 0){
+                L.marker(latlngs[index], {icon: startIcon}).addTo(map);
+            }else if(index+1 === latlngs.length){
+                L.marker(latlngs[index], {icon: endIcon}).addTo(map);
+            }else {
+                L.marker(latlngs[index], {icon: directionIcon, rotationAngle: angle}).addTo(map);
+            }
+        }
+
+        var polyline = L.polyline(latlngs, {
+                                                color: 'green',
+                                                weight: 8,
+                                                opacity: .7,
+                                            }).addTo(map);
 
         map.fitBounds(polyline.getBounds());
+
+        function getAngle(latLng1, latlng2, coef) {
+            var dy = latlng2[0] - latLng1[0];
+            var dx = Math.cos(Math.PI / 180 * latLng1[0]) * (latlng2[1] - latLng1[1]);
+            var ang = ((Math.atan2(dy, dx) / Math.PI) * 180 * coef);
+            return (ang).toFixed(2);
+        }
+
+        var decorator = L.polylineDecorator(polyline, {
+            patterns: [
+                {offset: 0, repeat: 40, symbol: L.Symbol.arrowHead({pixelSize: 30})}
+            ]
+        }).addTo(map);
     </script>
 @endpush
