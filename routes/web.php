4b8c2b3041910e0eb6c6e8467e3975ee0592fbd0
jadersonmr <jaderson.mrodrigues@gmail.com>
2026-04-17 19:03:35 -0300

    Corrigido o arquivo de rotas e adicionado ambiente Docker

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,22 @@ Route::group(['middleware' => 'auth', 'prefix' => 'produtos', 'as' => 'products.
     Route::get('', 'ProductController@index')->name('index');
     Route::get('/criar', 'ProductController@create')->name('create');
     Route::post('/criar', 'ProductController@store')->name('store');
-    Route::get('/{id}/produto', 'ProductController@show')->name('show')->where('id', '[0-9]+');;
-    Route::get('/{id}/editar', 'ProductController@edit')->name('edit')->where('id', '[0-9]+');;
-    Route::post('/{id}/editar', 'ProductController@update')->name('update')->where('id', '[0-9]+');;
-    Route::delete('/{id}/excluir', 'ProductController@destroy')->name('destroy')->where('id', '[0-9]+');;
+    Route::get('/{id}/produto', 'ProductController@show')->name('show')->where('id', '[0-9]+');
+    Route::get('/{id}/editar', 'ProductController@edit')->name('edit')->where('id', '[0-9]+');
+    Route::post('/{id}/editar', 'ProductController@update')->name('update')->where('id', '[0-9]+');
+    Route::delete('/{id}/excluir', 'ProductController@destroy')->name('destroy')->where('id', '[0-9]+');
     Route::any('/search', 'ProductController@search')->name('search');
 });
 
 Route::group(['middleware' => 'auth', 'prefix' => 'estoque', 'as' => 'stock.'], function() {
-    Route::get('/{id}', 'ProductStockController@index')->name('index')->where('id', '[0-9]+');;
-    Route::get('/entrada/{id}', 'ProductStockController@create')->name('create')->where('id', '[0-9]+');;
-    Route::get('/saida/{id}', 'ProductStockController@stockOutput')->name('output')->where('id', '[0-9]+');;
+    Route::get('/{id}', 'ProductStockController@index')->name('index')->where('id', '[0-9]+');
+    Route::get('/entrada/{id}', 'ProductStockController@create')->name('create')->where('id', '[0-9]+');
+    Route::get('/saida/{id}', 'ProductStockController@stockOutput')->name('output')->where('id', '[0-9]+');
     Route::post('/criar', 'ProductStockController@store')->name('store');
-    Route::get('/{id}/produto', 'ProductStockController@show')->name('show')->where('id', '[0-9]+');;
-    Route::get('/{id}/editar', 'ProductStockController@edit')->name('edit')->where('id', '[0-9]+');;
-    Route::post('/{id}/editar', 'ProductStockController@update')->name('update')->where('id', '[0-9]+');;
-    Route::delete('/{id}/excluir', 'ProductStockController@destroy')->name('destroy')->where('id', '[0-9]+');;
+    Route::get('/{id}/produto', 'ProductStockController@show')->name('show')->where('id', '[0-9]+');
+    Route::get('/{id}/editar', 'ProductStockController@edit')->name('edit')->where('id', '[0-9]+');
+    Route::post('/{id}/editar', 'ProductStockController@update')->name('update')->where('id', '[0-9]+');
+    Route::delete('/{id}/excluir', 'ProductStockController@destroy')->name('destroy')->where('id', '[0-9]+');
     Route::any('/search', 'ProductStockController@search')->name('search');
 });
 
